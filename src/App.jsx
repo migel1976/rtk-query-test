cd3d2a976199d199ad4c05caeab42e33b8525651
migel1976 <migel1976@gmail.com>
2024-08-03 17:08:20 +0300

    add method

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
-import { useGetGoodsQuery } from "./redux";
+import { useAddProductMutation, useGetGoodsQuery } from "./redux";
 
 const App = () => {
   const [count, setCount] = useState('')
   const { data, isLoading } = useGetGoodsQuery(count)
-  // const { data, isLoading } = useGetGoodsQuery()
+
+  const [addProduct, { isError }] = useAddProductMutation()
+  const [newProduct, setNewProduct] = useState('')
+
+
   if (isLoading) return <h1>...Loading</h1>
   const changeCount = (e) => {
     const value = e.target.value
@@ -12,9 +16,20 @@ const App = () => {
     setCount(value)
   }
 
+  const onNewProduct = async () => {
+    if (newProduct) {
+      await addProduct({ name: newProduct }).unwrap()
+      setNewProduct('')
+    }
+  }
+
   return (
     <>
       {/* <select value={count} onChange={(e) => setCount(e.target.value)}> */}
+      <diV>
+        <input type='text' value={newProduct} onChange={(e) => setNewProduct(e.target.value)} />
+      </diV>
+      <button onClick={onNewProduct}>add </button>
       <select value={count} onChange={(e) => changeCount(e)}>
         <option value=''>All</option>
         <option value='1'>1</option>
--- src/redux/goodsApi.js
+++ src/redux/goodsApi.js
@@ -4,15 +4,28 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const goodsApi = createApi({
   reducerPath: 'goodsApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001' }),
+  tagTypes: ['Product'],
   endpoints: (builder) => ({
     getGoods: builder.query({
       query: (limit = '') => {
         console.log(limit)
         return `goods?${limit && `_limit=${limit}`}`
-      }
+      },
+      providesTags: (result) =>
+        result
+          ? [...result.map(({ id }) => ({ type: 'Product', id })), 'Product']
+          : ['Product'],
+    }),
+    addProduct: builder.mutation({
+      query: (body) => ({
+        url: 'goods',
+        method: 'POST',
+        body
+      }),
+      invalidatesTags: ['Product'],
     })
   })
 })
 
-export const { useGetGoodsQuery } = goodsApi
+export const { useGetGoodsQuery, useAddProductMutation } = goodsApi
 
